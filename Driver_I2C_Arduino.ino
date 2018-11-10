ea35976e82147b22b547e75924d031f3a9441168
Miguel Orozco <39131275+Necrovalle@users.noreply.github.com>
2018-11-10 15:49:19 -0700

    Primera versión funcional del driver

---
+++ Driver_I2C_Arduino.ino
@@ -0,0 +1,201 @@
+/*********************************************
+ *          Firmware de driver I2C           *
+ *  Version: 0.8 a                           *
+ *  Fecha: Noviembre del 2018                *
+ *  Desarrollador: Miguel Orozco             *
+ *  Direccion del repositorio:               *
+ *  https://github.com/Necrovalle/Driver_I2C *
+ *********************************************/
+
+ //---------------- LIBRERIAS ----------------
+#include <Wire.h>
+
+//----------------- VARIABLES ----------------
+byte dirI2C = 0x01;       //Direccion de este driver como esclavo
+float verFirmware = 0.8;  //Version del firmware para consulta
+char msgSerial;           //Para lectura de la entrada del puerto serie
+int numChars;             //Cantidad de caracteres a mandar por I2C
+byte salidaI2C[2];        //Arreglo de los datos que se enviaran por I2C
+int contSalida;       //Contador de bytes de salida por I2C
+
+//--------------- CONFIGURACION --------------
+void setup() 
+{
+  //Activacion de servicios de comunicacion
+  Wire.begin(dirI2C);
+  Wire.onRequest(Peticion_Maestro);
+  Wire.onReceive(Escritura_Maestro);
+  Serial.begin(9600);
+
+  //Definicion de conexiones digitales
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+
+}
+
+//------------------- MAIN ------------------
+void loop() 
+{
+  //Atencion a la peticion de lectura/escritura de parametros 
+  if (Serial.available() > 0)
+  {
+    msgSerial = Serial.read();
+    switch (msgSerial)
+    {
+      case 'A':
+        Serial.println("-- I2C driver configuration --");
+        Serial.print("Address: 0x");
+        Serial.println(dirI2C, HEX);
+        Serial.print("Firmware version: ");
+        Serial.println(verFirmware);
+        break;
+
+       default:
+         Serial.println("* ERROR: command not found *");
+         break;
+    }
+    msgSerial = 'x';
+  }
+}
+
+//---------------- FUNCIONES ---------------
+void Peticion_Maestro()
+{
+  /*****************************************
+   *  Envio de datos a peticion del maetro *
+   *   con previa configuracion de valores *
+   * @param: Sin parametros                *
+   * @return: Sin retornos                 *
+   *****************************************/
+      Wire.write(salidaI2C[contSalida]);
+      contSalida=1;
+}
+
+
+void Escritura_Maestro(int numBytes)
+{
+  /*************************************************
+   *      Lectura de la petición del maestro       *
+   *        y ejecucion de tareas indicadas.       *
+   *        La codificacion de las tareas en       * 
+   *           el manual del repositorio.          *
+   * @param: numBytes es el numero de bytes a leer *
+   * @return: Sin retornos                         *
+   *************************************************/
+  byte entI2C;    //Dato recibido del maestro
+
+  while (Wire.available()>0)
+  {
+    entI2C = Wire.read();
+  }
+  contSalida = 0;
+
+  switch (entI2C)
+  {
+    case 0xA0:
+      Lectura_Analoga(0);
+      break;
+
+    case 0xA1:
+      Lectura_Analoga(1);
+      break;
+
+    case 0xA2:
+      Lectura_Analoga(2);
+      break;
+
+    case 0xA3:
+      Lectura_Analoga(3);
+      break;
+
+    case 0xD0:
+      Lectura_Digital(10);
+      break;
+
+    case 0xD1:
+      Lectura_Digital(11);
+      break;
+
+    case 0xE1:
+      Escritura_Digital(12, HIGH);
+      break;
+
+    case 0xF1:
+      Escritura_Digital(13, HIGH);
+      break;
+
+    case 0xE0:
+      Escritura_Digital(12, LOW);
+      break;
+
+    case 0xF0:
+      Escritura_Digital(13, LOW);
+      break;
+
+    default:
+      salidaI2C[0] = 0x00;
+      salidaI2C[1] = 0x00;
+      break;
+  }
+}
+
+
+void Lectura_Analoga(int canalAnalogo)
+{
+  /*****************************************************
+   *  Adquiere el valor del canal analogo seleccionado *
+   *   y prepara este valor para ser mandado por I2C   *
+   * @param: canalAnalogo es el identificador de pin   *
+   * @return: Sin retorno                              *
+   *****************************************************/
+  int voltajeMedido;    //Alojamiento del valo del ADC espesificado
+  int masSigni;         //byte mas significativo
+  int menSigni;         //byte menos significativo
+
+  voltajeMedido = analogRead(canalAnalogo);
+  //Cambio del valor de 10 bits a 2 byts
+  masSigni = (byte)(voltajeMedido/256);
+  menSigni = voltajeMedido - (masSigni * 256);
+  salidaI2C[0] = masSigni;
+  salidaI2C[1] = menSigni;
+}
+
+void Lectura_Digital(int pinLectura)
+{
+  /******************************************************
+   *  Adquire el valor dela entrada digital configurada *
+   *       y lo prepara para ser enviada por I2C        *
+   * @param: pinLectura es el numero de pin a ser leido *
+   * @return: Sin retorno                               *
+   ******************************************************/
+   boolean estadoDigital;   //Almacenamiento del estado digital leido
+   
+   estadoDigital = digitalRead(pinLectura);
+   if (estadoDigital)
+   {
+      salidaI2C[0] = 68;
+      salidaI2C[1] = 1;
+   }
+   else
+   {
+      salidaI2C[0] = 68;
+      salidaI2C[1] = 0;
+   }
+}
+
+void Escritura_Digital(int pinEscritura, boolean estadoSalida)
+{
+  /*******************************************************
+   *    Da salida al valor de estado logico espesificado *
+   *              en el canal espesificado               *
+   *  @param: pinEscritura es el pin de escritura        *
+   *          estadoSalida es el nivel logico a escribir *
+   *  @return: Sin retorno                               *
+   *******************************************************/
+   digitalWrite(pinEscritura, estadoSalida);
+   salidaI2C[0] = 65;
+   salidaI2C[1] = 75;
+}
+
---
+++ Probador_Driver_I2C_Arduino.ino
@@ -0,0 +1,143 @@
+/*********************************************
+ *  Firmware de prueva de driver I2C         *
+ *  Version: 0.4 a                           *
+ *  Fecha: Noviembre del 2018                *
+ *  Desarrollador: Miguel Orozco             *
+ *  Direccion del repositorio:               *
+ *  https://github.com/Necrovalle/Driver_I2C *
+ *********************************************/
+//---------------- LIBRERIAS ----------------
+#include <Wire.h>
+
+//----------------- VARIABLES ----------------
+byte salidaI2C;     //Caracter de salida por I2C
+byte entradaI2C;    //Caracter de entrada de i2C
+char entSerial;     //Caracter de entrada por puerto serie
+
+//--------------- CONFIGURACION --------------
+void setup() 
+{
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("Inicalizado");
+}
+
+//------------------- MAIN ------------------
+void loop() 
+{
+  if (Serial.available()>0)
+  {
+    entSerial = Serial.read();
+
+    switch (entSerial)
+    {
+      case 'A':
+        salidaI2C = 0xA0;
+        break;
+
+      case 'B':
+        salidaI2C = 0xA1;
+        break;
+
+      case 'C':
+        salidaI2C = 0xA2;
+        break;
+
+      case 'D':
+        salidaI2C = 0xA3;
+        break;
+
+      case 'E':
+        salidaI2C = 0xD0;
+        break;
+
+      case 'F':
+        salidaI2C = 0xD1;
+        break;
+
+      case 'G':
+        salidaI2C = 0xE1;
+        break;
+
+      case 'H':
+        salidaI2C = 0xF1;
+        break;
+
+      case 'g':
+        salidaI2C = 0xE0;
+        break;
+
+      case 'h':
+        salidaI2C = 0xF0;
+        break;
+
+      default:
+        Serial.println("Comando no encontrado");
+        salidaI2C = 0x00;
+        break;
+    }
+    Serial.println(salidaI2C);
+    Envio_I2C(salidaI2C);
+    delay(10);
+    Recepcion_I2C();
+  }
+}
+
+//---------------- FUNCIONES ---------------
+void Envio_I2C(byte codigoEnvio)
+{
+  /*************************************
+   *   Envio de dato espesificado al   *
+   *                esclavo            *
+   * @param:  codigoEnvio dat a enviar *
+   * @return: Sin retornos             *
+   *************************************/
+   Wire.beginTransmission(0x01); 
+   delay(50);
+   Wire.write(salidaI2C);
+   Wire.endTransmission();
+   delay(50);
+   Serial.print("Enviado ");
+   Serial.println(entSerial);
+}
+
+void Recepcion_I2C()
+{
+  /*************************************
+   *   Recepcion de dato espesificado al   *
+   *                esclavo            *
+   * @param:  ninguno *
+   * @return: Sin retornos             *
+   *************************************/
+   byte entI2C;   //Caracter leido
+   Wire.requestFrom(0x01, 1);
+   delay(150);
+   Serial.println("Recibiendo...");
+   while (Wire.available())
+   {
+      entI2C = Wire.read();
+      Serial.println(entI2C);
+   }
+   Wire.requestFrom(0x01, 1);
+   delay(5);
+   Wire.requestFrom(0x01, 1);
+   delay(5);
+   while (Wire.available())
+   {
+      entI2C = Wire.read();
+      Serial.println(entI2C);
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
